f1366e34da381c3a6ff98ce709eaf15002bd9e29
jurijsml <90174881+jurijsml@users.noreply.github.com>
2021-10-04 07:11:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+'''
+Jurijs Malahovskis
+04.10.2021
+'''
+
+def funkcums ():
+  print('Pirmā funkcija')
+funkcums()
+def funkcums (parcums):
+  print(funkcums, parcums)
+  funkcums(arguments)
+def funkcums (funkcums, gramatas):
+print(funkcums, parcums, gramatas)
